c20d0ff909e55e5d6ccb4e7309625c9f836667f8
sanjeev prabhugaonkar <sanjeev.prabhugaonkar@gmail.com>
2018-05-08 21:47:37 +0530

    Added PostUrl to connector package

--- app/src/main/java/example/sanjeev/com/quizapptrial/Connectors/PostUrl.java
+++ app/src/main/java/example/sanjeev/com/quizapptrial/Connectors/PostUrl.java
@@ -1,4 +1,4 @@
-package example.sanjeev.com.quizapptrial;
+package example.sanjeev.com.quizapptrial.Connectors;
 
 import android.content.Context;
 import android.support.v4.app.Fragment;
--- app/src/main/java/example/sanjeev/com/quizapptrial/ui/fragments/RegisterFragment.java
+++ app/src/main/java/example/sanjeev/com/quizapptrial/ui/fragments/RegisterFragment.java
@@ -10,11 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import java.util.HashMap;
 
-import example.sanjeev.com.quizapptrial.PostUrl;
+import example.sanjeev.com.quizapptrial.Connectors.PostUrl;
 import example.sanjeev.com.quizapptrial.R;
 
 public class RegisterFragment extends Fragment implements PostUrl.Interactor{
